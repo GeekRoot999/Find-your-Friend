0c453a2c58942fed8d06150b4ba55ba96950ed72
Aravind <40268272+GeekRoot999@users.noreply.github.com>
2020-06-04 19:11:21 +0530

    Updates

---
+++ precache-manifest.9f65973f3a30202fb474bef981d573da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eeb4d19d6c8609f5d014ffaa0a6d9a5",
+    "url": "/find-your-friends/index.html"
+  },
+  {
+    "revision": "339ba76c709e05b32b71",
+    "url": "/find-your-friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "19ddefb7c925aa3107e7",
+    "url": "/find-your-friends/static/css/main.ea5330cf.chunk.css"
+  },
+  {
+    "revision": "339ba76c709e05b32b71",
+    "url": "/find-your-friends/static/js/2.f9fe0209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/find-your-friends/static/js/2.f9fe0209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ddefb7c925aa3107e7",
+    "url": "/find-your-friends/static/js/main.3622e629.chunk.js"
+  },
+  {
+    "revision": "d1a7185dfdcb94db82ed",
+    "url": "/find-your-friends/static/js/runtime-main.a03cce43.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/find-your-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
